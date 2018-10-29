17945510041264c3decaa40f5cc987bdcd4d9060
beigid <beigid@seattleu.edu>
2018-10-29 16:59:24 -0700

    Fixed NPM build

---
+++ src/common/api.js
@@ -0,0 +1,18 @@
+// Base API Configuration
+
+import axios from 'axios';
+
+export const API = axios.create({
+    baseURL: '//api.openweathermap.org/data/2.5/'
+});
+
+API.interceptors.request.use(function (config) {
+    // Set common parameters for each request to the API.
+
+    config.params.APPID = '415abda2f5409b72a9e1d431897a0ac7';
+    config.params.units = 'imperial';
+
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueLocalStorage from 'vue-ls'
+
+let options = {
+  namespace: 'weather__'
+}
+
+Vue.use(VueLocalStorage, options);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
